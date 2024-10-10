545b5e69046b5847d578510354aa8a37667f74e9
sebelumcahahaya <anandadimmas1204@gmail.com>
2024-10-10 14:24:05 +0700

    user module

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -5,10 +5,10 @@ import { UserService } from './user.service';
 import { UserDao } from './user.dao';
 import { User } from './user.model';
 import { Profile } from '../profile/profile.model';
-import { Comment } from '../comment/comment.model'; // Import Comment model
+import { Comment } from '../comment/comment.model';
 
 @Module({
-  imports: [SequelizeModule.forFeature([User, Profile, Comment])], // Daftarkan Comment di sini
+  imports: [SequelizeModule.forFeature([User, Profile, Comment])],
   controllers: [UserController],
   providers: [UserService, UserDao],
 })
