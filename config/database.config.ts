34625223ba7ac3b11c9e908c324f31f15b978a36
sebelumcahahaya <anandadimmas1204@gmail.com>
2024-10-11 09:00:02 +0700

    add config

---
+++ config/database.config.ts
@@ -0,0 +1,27 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+console.log(process.env.DB_USERNAME, process.env.DB_PASSWORD, process.env.DB_NAME_DEV, process.env.DB_HOST, process.env.DB_DIALECT);
+export default {
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME_DEV,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+  },
+  test: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME_TEST,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+  },
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME_PROD,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+  },
+};
